588d5289b888477e5dc156d53140b47928aa168e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-09-19 22:28:20 +0900

    クイズ生成用のAPIをCDKで実装しました。その説明についてもREADMEに記載しました。

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,139 @@
+import * as cdk from 'aws-cdk-lib';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+import { Construct } from 'constructs';
+import path = require("path");
+
+/**
+ * SolanaRadarAPIServerStack
+ */
+export class SolanaRadarAPIServerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // SSMから環境変数を取得する。
+    const OPENAI_API_KEY = ssm.StringParameter.valueFromLookup(
+      this,
+      "OPENAI_API_KEY"
+    );
+
+    // Lambda関数を定義
+    const lambdaFunction = new NodejsFunction(this, "SolanaRadarAPILambdaFunction", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, "../resources/lambda/index.ts"),
+      handler: "handler",
+      bundling: {
+        forceDockerBundling: true,
+        nodeModules: ["hnswlib-node"],
+      },
+      timeout: cdk.Duration.seconds(600),
+      environment: {
+        OPENAI_API_KEY: OPENAI_API_KEY
+      },
+    });
+
+    // S3からファイルを取得するためのポリシー
+    const s3ReadPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: ['arn:aws:s3:::solana-radar-hackathon2024/*'], // 必要なS3バケット名に変更
+    });
+
+    // Lambda関数にS3アクセス権限を追加
+    lambdaFunction.addToRolePolicy(s3ReadPolicy);
+
+    // API Gateway Rest APIを作成
+    const api = new apigateway.RestApi(this, "SolanaRadarAPI", {
+      restApiName: "generateQuiz",
+      description: "SolanaRadarAPILambdaFunction servers my Lambda function.",
+    });
+
+    // Lambda Integration
+    const postLambdaIntegration = new apigateway.LambdaIntegration(
+      lambdaFunction,
+      {
+        requestTemplates: {
+          "application/json": '{ "statusCode": "200" }',
+        },
+        integrationResponses: [
+          {
+            statusCode: "200",
+            responseParameters: {
+              "method.response.header.Access-Control-Allow-Origin": "'*'",
+            },
+          },
+        ],
+      }
+    );
+
+    // APIキーを作成
+    const apiKey = api.addApiKey("ApiKey");
+
+    // APIキーを使用するUsagePlanを作成
+    const plan = api.addUsagePlan("UsagePlan", {
+      name: "Easy",
+      throttle: {
+        rateLimit: 10,
+        burstLimit: 2,
+      },
+    });
+
+    // APIのリソースとメソッドを定義
+    const items = api.root.addResource("generateQuiz");
+    // CORSの設定を追加
+    items.addCorsPreflight({
+      allowOrigins: apigateway.Cors.ALL_ORIGINS,
+      allowMethods: ["POST", "OPTIONS"],
+      allowHeaders: [
+        "Content-Type",
+        "X-Amz-Date",
+        "Authorization",
+        "X-Api-Key",
+        "X-Amz-Security-Token",
+      ],
+    });
+
+    const postMethod = items.addMethod("POST", postLambdaIntegration, {
+      apiKeyRequired: true,
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Origin": true,
+          },
+        },
+      ],
+    });
+
+    // UsagePlanにメソッドを追加
+    plan.addApiStage({
+      stage: api.deploymentStage,
+      throttle: [
+        {
+          method: postMethod,
+          throttle: {
+            rateLimit: 10,
+            burstLimit: 2,
+          },
+        },
+      ],
+    });
+    // UsagePlanにAPIキーを追加
+    plan.addApiKey(apiKey);
+
+    // 成果物
+    new cdk.CfnOutput(this, "SolanaRadarAPIUrl", {
+      value: api.url,
+      description: "The URL of the API Gateway",
+      exportName: "SolanaRadarAPIUrl",
+    });
+
+    new cdk.CfnOutput(this, "SolanaRadarAPILambdaFunctionArn", {
+      value: lambdaFunction.functionArn,
+      description: "The ARN of the Lambda function",
+      exportName: "SolanaRadarAPILambdaFunctionArn",
+    });
+  }
+}
---
+++ cdk/resources/lambda/index.ts
@@ -0,0 +1,117 @@
+import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
+import { Document } from "@langchain/core/documents";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import {
+  RunnableLambda,
+  RunnableMap,
+  RunnablePassthrough,
+} from "@langchain/core/runnables";
+import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { getS3Object } from "./helper/s3";
+
+// 環境変数を取得する。
+const {OPENAI_API_KEY} = process.env;
+
+/**
+ * ハンドラー
+ * @param event
+ * @returns
+ */
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  
+  console.log(`
+    ================================ [START] ================================
+  `);
+
+  // S3バケット名を指定
+  const bucketName = 'solana-radar-hackathon2024'; 
+  // ファイル名を指定
+  const objectKey = 'MagicBlock.md'; 
+
+  const content = await getS3Object(bucketName, objectKey);
+  // ベクトルデータストア
+  const vectorStore = await HNSWLib.fromDocuments(
+    [new Document({pageContent: content})],
+    new OpenAIEmbeddings()
+  );
+
+  const retriever = vectorStore.asRetriever(1);
+  // テンプレートプロンプト
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      "ai",
+      `Please create simple question based on only the following context:
+        
+      {context}`,
+    ],
+    ["human", "{question}"],
+  ]);
+
+  // モデルを指定
+  const model = new ChatOpenAI({
+    apiKey: OPENAI_API_KEY!,
+  });
+  const outputParser = new StringOutputParser();
+
+  const setupAndRetrieval = RunnableMap.from({
+    context: new RunnableLambda({
+      func: (input: string) =>
+        retriever.invoke(input).then((response) => response[0].pageContent),
+    }).withConfig({runName: "contextRetriever"}),
+    question: new RunnablePassthrough(),
+  });
+
+  let response;
+
+  try {
+    // プロンプトチェーンを作成
+    const chain = setupAndRetrieval.pipe(prompt).pipe(model).pipe(outputParser);
+    // プロンプトを実行
+    const aiResponse = await chain.invoke(`
+        MagicBookについて簡単なクイズを作成してください。
+        
+        その際、回答は4択して正しい答えが1つだけになるようにしてください。
+        問題と回答は1ペアだけ作成してください。
+        問題文に答えが含まれないように注意してください。
+
+        問題と回答はJSON形式で出力してください。
+    `);
+    console.log("aiResponse:::", aiResponse);
+
+    response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify({
+        content: aiResponse,
+      }),
+    };
+  } catch (e: any) {
+    console.error("error: ", e);
+
+    response = {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify({
+        message: "send meta tx failed.",
+      }),
+    };
+  } finally {
+    console.log(`
+      ================================ [END] ================================
+    `);
+  }
+
+  return response;
+}
