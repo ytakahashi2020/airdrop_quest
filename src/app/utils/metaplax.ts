34eba0f642660f0bfa7b0cbde471a17afdb23737
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-10-06 20:25:17 +0900

    修正

--- src/app/utils/metaplax.ts
+++ src/app/utils/metaplax.ts
@@ -4,7 +4,22 @@
 export const mintWinnerNFT = async () => {
   try {
     // NFTのメタデータを生成するAPIを呼びだす。
-    const response = await fetch("/api/metaplex");
+    const response = await fetch("/api/nft/mint/winnerNft");
+    console.log("response", response);
+    return response;
+  } catch (error) {
+    console.error("MetaPlext API呼び出し中にエラーが発生しました:", error);
+    return null;
+  }
+}
+
+/**
+ * mint Statue method
+ */
+export const mintStatueNFT = async () => {
+  try {
+    // NFTのメタデータを生成するAPIを呼びだす。
+    const response = await fetch("/api/nft/mint/statue");
     console.log("response", response);
     return response;
   } catch (error) {
